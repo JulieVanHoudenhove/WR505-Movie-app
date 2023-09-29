bc3c6a52e4b4ce6094bebfaafa268d52de1c42e1
Julie Van Houdenhove <100696436+JulieVanHoudenhove@users.noreply.github.com>
2023-09-29 12:20:42 +0200

    feat : add page detailed

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,8 @@ import HomeView from '../views/HomeView.vue'
 import MoviesView from '../views/MoviesView.vue'
 import ActorsView from '../views/ActorsView.vue'
 import CategoriesView from '../views/CategoriesView.vue'
+import DetailMoviesView from '../views/DetailMoviesView.vue'
+import DetailActorsView from '../views/DetailActorsView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -21,9 +23,22 @@ const router = createRouter({
       component: MoviesView
     },
     {
+      path: '/movie/:id',
+      name: 'detailmovie',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: DetailMoviesView
+    },
+    {
       path: '/actors',
       name: 'actor',
       component: ActorsView
+    },
+    {
+      path: '/actor/:id',
+      name: 'detailactor',
+      component: DetailActorsView
     },
     {
       path: '/categories',
---
+++ src/views/DetailActorsView.vue
@@ -0,0 +1,30 @@
+<script setup>
+import { useRoute } from 'vue-router'
+import {onMounted, ref} from "vue";
+import Movie from "@/components/Movie.vue";
+
+const route = useRoute()
+
+// Pour récupérer un paramètre de l'URL
+const id = route.params.id
+
+let actor = ref('')
+
+onMounted(async () => {
+  const response = await fetch('http://localhost:8000/api/actors/' + id)
+  actor.value = await response.json()
+})
+</script>
+
+<template>
+  <h1>Oui</h1>
+  <h1>Fiche Film n°{{ actor.id }}</h1>
+  <h3>{{ actor.firstName ? actor.firstName : 'loading...' }}</h3>
+  <p>{{ actor.lastName ? actor.lastName : 'loading...' }}</p>
+  <p>{{ actor.nationality.nationality }}</p>
+  <div v-for="movies in actor.movies">
+    <Movie :movie="movies" />
+  </div>
+</template>
+
+<style></style>
