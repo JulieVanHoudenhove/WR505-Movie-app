58873ddb1562b22834e02d8318121077be838af4
Julie Van Houdenhove <100696436+JulieVanHoudenhove@users.noreply.github.com>
2023-09-29 12:15:57 +0200

    feat : add actors and movies on homepage & create components card actor and card movie

---
+++ src/components/Actor.vue
@@ -0,0 +1,20 @@
+<script setup>
+
+defineProps({
+  actor: {
+    type: Object,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <div>
+    <router-link :to="`/actor/`+ actor.id">
+      <img src="https://source.unsplash.com/random/150x200/?actor">
+      <p>{{ actor.firstName }} {{ actor.lastName }}</p>
+    </router-link>
+  </div>
+</template>
+
+<style scoped></style>
---
+++ src/components/Movie.vue
@@ -0,0 +1,23 @@
+<script setup>
+
+defineProps({
+  movie: {
+    type: Object,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <div>
+    <router-link :to="`/movie/`+ movie.id">
+      <img src="https://source.unsplash.com/random/150x200/?film">
+      <h2>{{ movie.title }}</h2>
+      <p>{{ movie.description }}</p>
+      <p>{{ movie.duration }}</p>
+    </router-link>
+  </div>
+</template>
+
+<style scoped></style>
+```
