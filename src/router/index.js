8128973a8ed0a2f87d2d255379882c6512f272c5
Julie Van Houdenhove <100696436+JulieVanHoudenhove@users.noreply.github.com>
2023-09-29 09:00:46 +0200

    feat : create navigation and views

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import MoviesView from '../views/MoviesView.vue'
+import ActorsView from '../views/ActorsView.vue'
+import CategoriesView from '../views/CategoriesView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -10,12 +13,22 @@ const router = createRouter({
       component: HomeView
     },
     {
-      path: '/about',
-      name: 'about',
+      path: '/movies',
+      name: 'movies',
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+      component: MoviesView
+    },
+    {
+      path: '/actors',
+      name: 'actor',
+      component: ActorsView
+    },
+    {
+      path: '/categories',
+      name: 'categories',
+      component: CategoriesView
     }
   ]
 })
