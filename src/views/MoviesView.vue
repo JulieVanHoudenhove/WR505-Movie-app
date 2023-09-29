be40fda7f3cd80ffa878cf278ea261539eb5d1ad
Julie Van Houdenhove <100696436+JulieVanHoudenhove@users.noreply.github.com>
2023-09-29 12:27:03 +0200

    feat : add pages with all the movies and actors

--- src/views/MoviesView.vue
+++ src/views/MoviesView.vue
@@ -1,6 +1,32 @@
 <script setup>
+import { onMounted, ref } from 'vue'
+import Movie from '@/components/Movie.vue'
+
+let movies = ref([])
+
+onMounted(async () => {
+  const movieResponse = await fetch('http://localhost:8000/api/movies?page=1', {
+    headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json'
+    }
+  })
+  movies.value = await movieResponse.json()
+})
 </script>
 
-<template></template>
+<template>
+  <h1 class="text-2xl font-bold">Tous les films</h1>
+  <div class="my-xl">
+    <div class="grid grid-cols-4">
+      <div v-if="movies" v-for="movie in movies">
+        <Movie :movie="movie" />
+      </div>
+      <div v-else>
+        <p>Loading...</p>
+      </div>
+    </div>
+  </div>
+</template>
 
-<style scoped></style>
+<style></style>
