ae80b3e5bf354c16b5c213b8a08a2d7bfc608aab
Julie Van Houdenhove <100696436+JulieVanHoudenhove@users.noreply.github.com>
2024-02-25 12:58:09 +0100

    feat: add pagination

--- src/views/MoviesView.vue
+++ src/views/MoviesView.vue
@@ -9,32 +9,87 @@ if (!token) {
 }
 
 let movies = ref([])
-
-onMounted(async () => {
-  const movieResponse = await fetch('http://localhost:8000/api/movies?page=1', {
-    headers: {
-      'Content-Type': 'application/json',
-      'Accept': 'application/json',
-      'Authorization': 'Bearer ' + token
-    }
-  })
-  if (movieResponse.status === 401) {
-    localStorage.removeItem('token')
-    router.push('/login')
-  }
-  movies.value = await movieResponse.json()
-
-  filter()
-})
-
 let resultat = ref([])
 let recherche = ref('')
+let pageNext = ref('')
+let pagePrevious = ref('')
 
-let filter = () => {
+onMounted(async () => {
+  await getMovies()
+})
+
+async function getMovies() {
+  try {
+    const response = await fetch('http://localhost:8000/api/movies', {
+      headers: {
+        'Authorization': 'Bearer ' + token
+      }
+    })
+    const data = await response.json()
+    if (data.code === 401) {
+      localStorage.removeItem('token')
+      return router.push('/login')
+    } else {
+      movies.value = data['hydra:member'];
+      pageNext.value = data['hydra:view']['hydra:next'];
+      pagePrevious.value = data['hydra:view']['hydra:previous'];
+    }
+    filter()
+  } catch (error) {
+    console.error('Une erreur s\'est produite', error)
+  }
+}
+
+async function filter() {
   resultat.value = movies.value
       .map((movie, index) => ({ movie, index }))
       .filter(({ movie }) => movie.title.toLowerCase().includes(recherche.value.toLowerCase()));
 }
+
+async function previousPage() {
+  try {
+    const response = await fetch(`http://localhost:8000${pagePrevious.value}`, {
+      headers: {
+        'Authorization': 'Bearer ' + token
+      }
+    })
+    const data = await response.json()
+    if (data.code === 401) {
+      localStorage.removeItem('token')
+      return router.push('/login')
+    } else {
+      movies.value = data['hydra:member'];
+      pageNext.value = data['hydra:view']['hydra:next'];
+      pagePrevious.value = data['hydra:view']['hydra:previous'];
+      filter()
+    }
+  } catch (error) {
+    console.error('Une erreur s\'est produite', error)
+  }
+}
+
+async function nextPage() {
+  try {
+    const response = await fetch(`http://localhost:8000${pageNext.value}`, {
+      headers: {
+        'Authorization': 'Bearer ' + token
+      }
+    })
+    const data = await response.json()
+    if (data.code === 401) {
+      localStorage.removeItem('token')
+      return router.push('/login')
+    } else {
+      movies.value = data['hydra:member'];
+      pageNext.value = data['hydra:view']['hydra:next'];
+      pagePrevious.value = data['hydra:view']['hydra:previous'];
+      filter()
+    }
+  } catch (error) {
+    console.error('Une erreur s\'est produite', error)
+  }
+}
+
 </script>
 
 <template>
@@ -50,6 +105,12 @@ let filter = () => {
         <p>Loading...</p>
       </div>
     </div>
+  </div>
+  <div class="flex justify-between">
+    <button v-if="pagePrevious" @click="previousPage()">Précédent</button>
+    <button v-else disabled>Précédent</button>
+    <button v-if="pageNext" @click="nextPage()">Suivant</button>
+    <button v-else disabled>Suivant</button>
   </div>
 </template>
 
