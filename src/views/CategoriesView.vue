053ce725f53ed7084c05c84575bd39dd9a293760
Julie Van Houdenhove <100696436+JulieVanHoudenhove@users.noreply.github.com>
2024-02-29 21:22:00 +0100

    feat: rework fetch categories, do research and pagination

--- src/views/CategoriesView.vue
+++ src/views/CategoriesView.vue
@@ -7,49 +7,131 @@ if (!token) {
   router.push('/login')
 }
 
+const toggleForm = ref(false)
+
+const decodeToken = JSON.parse(atob(token.split('.')[1]));
+const roles = decodeToken.roles[0];
+
 let categories = ref([])
+let recherche = ref('')
+let nextPage = ref('')
+let previousPage = ref('')
 
 onMounted(async () => {
-  const categoryResponse = await fetch('http://localhost:8000/api/categories', {
-    headers: {
-      'Content-Type': 'application/json',
-      'Accept': 'application/json',
-      'Authorization': 'Bearer ' + token
-    }
-  })
-  if (categoryResponse.status === 401) {
-    localStorage.removeItem('token')
-    router.push('/login')
-  }
-  categories.value = await categoryResponse.json()
-  console.log(categories.value)
-
-  filter()
+  await getCategories()
 })
 
-let resultat = ref([])
-let recherche = ref('')
-
-let filter = () => {
-  resultat.value = categories.value
-      .map((category, index) => ({ category, index }))
-      .filter(({ category }) => category.name.toLowerCase().includes(recherche.value.toLowerCase()));
+async function getCategories() {
+  try {
+    const response = await fetch('http://localhost:8000/api/categories', {
+      headers: {
+        'Authorization': 'Bearer ' + token
+      }
+    })
+    const data = await response.json()
+    if (data.code === 401) {
+      localStorage.removeItem('token')
+      return router.push('/login')
+    } else {
+      categories.value = data['hydra:member'];
+      nextPage.value = data['hydra:view']['hydra:next'];
+      previousPage.value = data['hydra:view']['hydra:previous'];
+    }
+  } catch (error) {
+    console.error('Une erreur s\'est produite', error)
+  }
 }
+
+async function searchCategory() {
+  try {
+    const response = await fetch('http://localhost:8000/api/categories?name=' + recherche.value, {
+      headers: {
+        'Authorization': 'Bearer ' + token
+      }
+    })
+    const data = await response.json()
+    if (data.code === 401) {
+      localStorage.removeItem('token')
+      return router.push('/login')
+    } else {
+      categories.value = data['hydra:member'];
+      nextPage.value = data['hydra:view']['hydra:next'];
+      previousPage.value = data['hydra:view']['hydra:previous'];
+    }
+  } catch (error) {
+    console.error('Une erreur s\'est produite', error)
+  }
+}
+
+async function pagePrevious() {
+  try {
+    const response = await fetch('http://localhost:8000' + previousPage.value, {
+      headers: {
+        'Authorization': 'Bearer ' + token
+      }
+    })
+    const data = await response.json()
+    if (data.code === 401) {
+      localStorage.removeItem('token')
+      return router.push('/login')
+    } else {
+      categories.value = data['hydra:member'];
+      nextPage.value = data['hydra:view']['hydra:next'];
+      previousPage.value = data['hydra:view']['hydra:previous'];
+    }
+  } catch (error) {
+    console.error('Une erreur s\'est produite', error)
+  }
+}
+
+async function pageNext() {
+  try {
+    const response = await fetch('http://localhost:8000' + nextPage.value, {
+      headers: {
+        'Authorization': 'Bearer ' + token
+      }
+    })
+    const data = await response.json()
+    if (data.code === 401) {
+      localStorage.removeItem('token')
+      return router.push('/login')
+    } else {
+      categories.value = data['hydra:member'];
+      nextPage.value = data['hydra:view']['hydra:next'];
+      previousPage.value = data['hydra:view']['hydra:previous'];
+    }
+  } catch (error) {
+    console.error('Une erreur s\'est produite', error)
+  }
+}
+
 </script>
 
 <template>
-  <div>
-    <h1>Categories</h1>
-    <input placeholder="search a category" type="text" v-model="recherche" @input="filter" class="border-b">
-    <button @click="filter">Recherche</button>
+  <h1>Toutes les categories</h1>
+  <div class="my-xl">
+    <input placeholder="search a category" type="text" v-model="recherche" @input="searchCategory" class="border-b">
+    <button @click="searchCategory">Recherche</button>
+    <div>
+      <button v-if="roles === 'ROLE_ADMIN'" @click="toggleForm = true">Ajouter une catégorie</button>
+      <div v-if="toggleForm">
+        <CreateCategoryView @updateCategory="getCategories(); toggleForm = false" />
+      </div>
+    </div>
     <div class="grid grid-cols-4">
-      <div v-if="categories" v-for="category in resultat">
-        <p>{{ category.category.name }}</p>
+      <div v-if="categories" v-for="category in categories">
+        <p>{{ category.name }}</p>
       </div>
       <div v-else>
         <p>Loading...</p>
       </div>
     </div>
+  </div>
+  <div class="flex justify-between">
+    <button v-if="previousPage" @click="pagePrevious()">Précédent</button>
+    <button v-else disabled>Précédent</button>
+    <button v-if="nextPage" @click="pageNext()">Suivant</button>
+    <button v-else disabled>Suivant</button>
   </div>
 </template>
 
