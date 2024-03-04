f3d83fadeef2f7cf39adf39ad13888f985445ba5
Julie Van Houdenhove <100696436+JulieVanHoudenhove@users.noreply.github.com>
2024-03-04 10:38:11 +0100

    feat: style and responsive detail views

--- src/views/DetailMoviesView.vue
+++ src/views/DetailMoviesView.vue
@@ -20,8 +20,6 @@ let movie = ref('')
 onMounted(async () => {
   const response = await fetch('http://localhost:8000/api/movies/' + id, {
     headers: {
-      'Content-Type': 'application/json',
-          'Accept': 'application/json',
           'Authorization': 'Bearer ' + token
     }
   })
@@ -34,17 +32,22 @@ onMounted(async () => {
 </script>
 
 <template>
-  <h1>Détail du film</h1>
-  <h1>Fiche Film n°{{ movie.id }}</h1>
-  <h3>Titre : {{ movie.title ? movie.title : 'loading...' }}</h3>
-  <p>Description : {{ movie.description ? movie.description : 'loading...' }}</p>
-  <p>Temps du film :{{ movie.duration ? movie.duration : 'loading...' }}</p>
-  <p>Date de sortie : {{ moment(movie.releaseDate).format('DD/MM/YYYY') }}</p>
-  <p>Sorti il y a {{ moment().diff(moment(movie.releaseDate), "month") }} mois</p>
-  <div v-if="movie.actor">
-    Les acteurs du film :
-    <div v-for="actors in movie.actor">
-      <Actor :actor="actors" />
+  <div class="px-20 lg:px-30 xl:px-40">
+    <h1 class="text-4xl text-center p-10 font-bold">Détail du film</h1>
+    <div class="flex flex-col items-center mb-5">
+      <p><span class="font-bold text-lg">Titre : </span>{{ movie.title ? movie.title : 'loading...' }}</p>
+      <p class="text-center"><span class="font-bold text-lg">Description : </span>{{ movie.description ? movie.description : 'loading...' }}</p>
+      <p class="text-center"><span class="font-bold text-lg">Temps du film : </span>{{ movie.duration ? movie.duration : 'loading...' }}</p>
+      <p class="text-center"><span class="font-bold text-lg">Date de sortie : </span>{{ moment(movie.releaseDate).format('DD/MM/YYYY') }}</p>
+      <p><span class="font-bold text-lg">Sorti il y a : </span>{{ moment().diff(moment(movie.releaseDate), "month") }} mois</p>
+    </div>
+    <div class="flex flex-col items-center gap-7" v-if="movie.actor">
+      <p class="font-bold text-lg">Les acteurs du film : </p>
+      <div class="w-full flex flex-col justify-center items-center 2xl:grid 2xl:grid-cols-2 gap-10">
+        <div v-for="actor in movie.actor">
+          <Actor :actor="actor" />
+        </div>
+      </div>
     </div>
   </div>
 </template>
